3db4f10b8c447b68aab65fb9b58dfa76494230dd
emhernandezva <emhernandezva@unal.edu.co>
2022-11-25 21:30:59 -0500

    Intento 6

--- preguntas.py
+++ preguntas.py
@@ -1,18 +1,15 @@
 """
 Clasificación usando k-NN - Digits Dataset
 -----------------------------------------------------------------------------------------
-
 En este laboratio se construirá un clasificador usando k-NN para el dataset de digitos.
-
 """
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn import datasets
 
+
 def pregunta_01():
-    import pandas as pd
-    from sklearn import datasets
     """
     Complete el código presentado a continuación.
     """
@@ -29,14 +26,11 @@ def pregunta_01():
     # Imprima las dimensiones del vector de salida
     print(digits.target.shape)
 
-
 def pregunta_02():
-    import pandas as pd
-    from sklearn import datasets
     """
     Complete el código presentado a continuación.
     """
-    # Importe KNeighborsClassifier de sklearn.neighbors
+     # Importe KNeighborsClassifier de sklearn.neighbors
     from sklearn.neighbors import KNeighborsClassifier
 
     # Importe train_test_split de sklearn.model_selection
@@ -60,15 +54,12 @@ def pregunta_02():
     knn = KNeighborsClassifier(n_neighbors=7)
 
     # Entrene el clasificador
-    knn.fit(X, y)
+    knn.fit(X_train, y_train)
 
     # Imprima la precisión (score) del clasificador en el conjunto de datos de prueba
-    print(round(knn.score(X, y), 4))
-
+    print(round(knn.score(X_test, y_test), 4))
 
 def pregunta_03():
-    import pandas as pd
-    from sklearn import datasets
     """
     Complete el código presentado a continuación.
     """
@@ -88,7 +79,6 @@ def pregunta_03():
 
     # Divida los datos de entrenamiento y prueba. Los conjuntos de datos están
     # estratificados. La semilla del generador de números aleatorios es 42.
-    # El tamaño del test es del 20%
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42, stratify=y
     )
@@ -105,10 +95,10 @@ def pregunta_03():
         knn = KNeighborsClassifier(n_neighbors=k)
 
         # Entrene el clasificador con los datos de entrenamiento
-        knn.fit(X, y)
+        knn.fit(X_train,y_train)
 
         # Calcule la precisión para el conjunto de datos de entrenamiento
-        train_accuracy[i] = knn.score(X_train, y_train)
+        train_accuracy[i] = knn.score(X_train,y_train)
 
         # Calcule la precisión para el conjunto de datos de prueba
         test_accuracy[i] = knn.score(X_test, y_test)
